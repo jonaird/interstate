f26d8120ef56bfad841d45573497791471bc8f2a
jonaird <jonathan.aird@gmail.com>
2024-12-26 00:07:28 -0600

    initial commit

---
+++ bin/interstate.dart
@@ -0,0 +1,5 @@
+import 'package:interstate/interstate.dart' as interstate;
+
+void main(List<String> arguments) {
+  print('Hello world: ${interstate.calculate()}!');
+}
---
+++ test/interstate_test.dart
@@ -0,0 +1,6 @@
+import 'package:interstate/interstate.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('', () {});
+}
